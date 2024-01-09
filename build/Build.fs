2c23f865fb18a1ec91c1f203b42121e8a59ffaff
Petr Chromec <petr.chromec@lmc.eu>
2024-01-09 16:06:42 +0100

    Use net8.0

--- build/Build.fs
+++ build/Build.fs
@@ -1,5 +1,5 @@
 // ========================================================================================================
-// === F# / Project fake build ==================================================================== 1.1.0 =
+// === F# / Project fake build ==================================================================== 1.3.0 =
 // --------------------------------------------------------------------------------------------------------
 // Options:
 //  - no-clean   - disables clean of dirs in the first step (required on CI)
@@ -23,7 +23,9 @@ let main args =
             Summary = "Library for handling internal memory state (_concurrently, etc._)."
             Git = Git.init ()
         }
-        Specs = Spec.defaultLibrary
+        Specs =
+            Spec.defaultLibrary
+            |> Spec.mapLibrary (fun library -> { library with NugetApi = NugetApi.Organization "almacareer" })
     }
 
     args |> Args.run
